f6a280e9027360bc175e2dd168f8f54afdefeae1
كيرلس فرج <103357706+Matrixs1@users.noreply.github.com>
2023-04-04 10:05:05 +0200

    Add files via upload

---
+++ cv/js/main.js
@@ -0,0 +1,90 @@
+const asd1 = document.querySelector('.asd1');
+const asd2 = document.querySelector('.asd2');
+const asd3 = document.querySelector('.asd3');
+const asd4 = document.querySelector('.asd4');
+const asd5 = document.querySelector('.asd5');
+const asd6 = document.querySelector('.asd6');
+const asd7 = document.querySelector('.asd7');
+
+
+
+if (asd1.style.width != 100) {
+    
+    asd1.classList.add('val');
+    setTimeout(() => {
+        asd1.classList.remove('val');
+        
+    }, 1000);
+    asd1.classList.add('val2');
+    
+}
+
+if (asd2.style.width != 100) {
+    
+    asd2.classList.add('val');
+    setTimeout(() => {
+        asd2.classList.remove('val');
+        
+    }, 1000);
+    asd2.classList.add('val2');
+    
+}
+
+
+if (asd3.style.width != 100) {
+    
+    asd3.classList.add('val');
+    setTimeout(() => {
+        asd3.classList.remove('val');
+        
+    }, 1000);
+    asd3.classList.add('val2');
+    
+}
+
+if (asd4.style.width != 100) {
+    
+    asd4.classList.add('val');
+    setTimeout(() => {
+        asd4.classList.remove('val');
+        
+    }, 1000);
+    asd4.classList.add('val2');
+    
+}
+
+
+if (asd5.style.width != 100) {
+    
+    asd5.classList.add('val1');
+    setTimeout(() => {
+        asd5.classList.remove('val1');
+        
+    }, 1000);
+    asd5.classList.add('val2');
+    
+}
+
+
+if (asd6.style.width != 100) {
+    
+    asd6.classList.add('val1');
+    setTimeout(() => {
+        asd6.classList.remove('val1');
+        
+    }, 1000);
+    asd6.classList.add('val2');
+    
+}
+
+
+if (asd7.style.width != 100) {
+    
+    asd7.classList.add('val1');
+    setTimeout(() => {
+        asd7.classList.remove('val1');
+        
+    }, 1000);
+    asd7.classList.add('val2');
+    
+}
